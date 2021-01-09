2fec3fce1757bde8ad0f212b67b4515d2d986834
TomSenter <trs1@icloud.com>
2021-01-09 13:09:01 +0000

    add css and js for App component

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -2,7 +2,18 @@
 import './App.css';
 
 function App() {
-  return <h1>Hello</h1>
+  return (
+<div>
+  <h1>Ja<span class="highlight">mmm</span>ing</h1>
+  <div class="App">
+    <!-- Add a SearchBar component -->
+    <div class="App-playlist">
+      <!-- Add a SearchResults component -->
+      <!-- Add a Playlist component -->
+    </div>
+  </div>
+</div>
+  );
 }
 
 export default App;
